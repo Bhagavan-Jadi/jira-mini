9932de6d5b7736b26897ef14106a580d8d4beede
Bhagavan <jadibhagavan7@gmail.com>
2026-02-18 22:52:12 +0530

    Add user entity and user repository with ManyToMany role Mapping

---
+++ src/main/java/com/jadi/jira_mini/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jadi.jira_mini.repository;
+
+import com.jadi.jira_mini.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
